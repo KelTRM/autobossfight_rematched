771144e48f6b030071c0a7acd9d6906c52ba6c04
Kelvin Hansen <kelvin@keltrm.com>
2026-06-04 13:18:05 -0500

    update struct Entity to better reflect original typescript IEntity

--- entity.h
+++ entity.h
@@ -6,12 +6,12 @@ typedef uint64_t Energy_t;
 typedef uint64_t Health_t;
 
 struct Entity {
-	uint64_t EntityID;
+	char *Name;
+	Health_t HealthPoints;
+	//uint64_t EntityID;
 
-	Health_t HealingMinimum;
-	Health_t HealingMaximum;
+	//Health_t HealingMinimum;
+	//Health_t HealingMaximum;
 
-	Energy_t Energy;
-
-
+	//Energy_t Energy;
 };
