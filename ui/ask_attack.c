3a3b926cf7f59a3c1a3dcb236695bacec21c707a
Kelvin Hansen <kelvin@keltrm.com>
2026-06-10 20:57:13 -0500

    add sleep to askattack to prevent flickering

--- ui/ask_attack.c
+++ ui/ask_attack.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include"../entity.h"
 #include"console_manager/console.h"
+#include"../utils/sleep.h"
 
 int RefreshScoreboard(int PreserveCursorPosition);
 
 int AskAttack(Entity_t CurrentPlayer, uint64_t Round) {
 	ClearScreen();
 	RefreshScoreboard(0);
-	printf("Not implemented.\n");
+	sleep(2500);
+	//printf("Not implemented.\n");
 	return 0;
 }
