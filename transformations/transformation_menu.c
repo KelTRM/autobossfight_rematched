2c3d491192836f812d0ae117b8c4ac9ef00c0185
Kelvin Hansen <kelvin@keltrm.com>
2026-06-28 21:05:07 -0500

    fix AskTransformation

--- transformations/transformation_menu.c
+++ transformations/transformation_menu.c
@@ -1,14 +1,26 @@
 #include"transformation_manager.h"
 #include<ui.h>
 
-Transformation_t *AskTransformation() {
+Transformation_t *AskTransformation(void) {
+	BUFHANDLE MainBuffer = ActiveBuffer;
+	BUFHANDLE Buffer = CreateBuffer();
+
 	TransformationIter_t Iterator = OpenTransformationIterator();
 	Transformation_t *CurrentTransformation;
 
-	while ((CurrentTransformation=StepTransformationIterator(&Iterator)) != NULL) {
-		printf("[%s] (%lu%%/round. %lu upfront\n",
-				CurrentTransformation->Name,
-				CurrentTransformation->Upkeep,
-				CurrentTransformation->Cost);
+	while (Iterator != NULL) {
+		CurrentTransformation = StepTransformationIterator(&Iterator);
+		bprintf(Buffer, "[%s] (%lu cost. %lu%% upkeep)\n",
+			CurrentTransformation->Name,
+			CurrentTransformation->Cost,
+			CurrentTransformation->Upkeep
+		);
 	}
+
+	SwitchBuffer(Buffer);
+
+	char *Response;
+	Prompt("Select a translation.", &Response, 0);
+
+	SwitchBuffer(MainBuffer);
 }
