078908b32117430434b8aaf870df20f3e84de328
Kelvin Hansen <kelvin@keltrm.com>
2026-07-04 23:51:56 -0500

    implement AttachBufferFile

--- ui/console_manager/attach_buffer_file.c
+++ ui/console_manager/attach_buffer_file.c
@@ -2,5 +2,10 @@
 #include"buffer.h"
 
 void AttachBufferFile(BUFHANDLE Buffer, FILE *Destination, int CloseOnDeletion) {
-	
+	int Result = ValidateBuffer(Buffer);
+	if (Result == 0)
+		return;
+
+	Buffers[Buffer].BufferDestination = Destination;
+	Buffers[Buffer].CloseFile = CloseOnDeletion;
 }
