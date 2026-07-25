94aa087f057adc08f71457e2a7598c357077a16e
Kelvin Hansen <kelvin@keltrm.com>
2026-07-24 23:17:18 -0500

    note: known memory bug with lua strings

--- loadlua/attacks/define_attack_plugin.c
+++ loadlua/attacks/define_attack_plugin.c
@@ -1,5 +1,7 @@
+#include <assert.h>
 #include<lua.h>
 #include<lauxlib.h>
+#include"../../debug/debug.h"
 
 /*
  * -- ATTACK TABLE --
@@ -13,19 +15,93 @@
 }
 */
 
+void VerifyField(lua_State *L, const char *Name, int ExpectedType);
+const char *ReadLuaTableString(lua_State *L, const char *Name, char *DefaultValue);
+lua_Number ReadLuaTableNumber(lua_State *L, const char *Name, lua_Number DefaultValue);
+
 int AddAttack(lua_State *L) {
+	return 0;
+//	return 1;
 	int n = lua_gettop(L);
 	if (n != 3) {
 		lua_pushliteral(L, "AddAttack: expected AttackPlugin:AddAttack(string, table)");
 		lua_error(L);
 	}
 
+	int type = lua_type(L, -2);
+	if (type != LUA_TSTRING) {
+		lua_pushfstring(L, "expected type string (got %s instead)", lua_typename(L, type));
+		lua_error(L);
+	}
+
+	type = lua_type(L, -1);
+	if (type != LUA_TTABLE) {
+		lua_pushfstring(L, "expected type table (got %s instead)", lua_typename(L, type));
+		lua_error(L);
+	}
+
+//	return 0;
+
+	const char *AttackName = lua_tostring(L, -2);
+
+	const char *AttackDispName = ReadLuaTableString(L, "AttackName", "nil");
+	lua_Number RequiredEnergy = ReadLuaTableNumber(L, "RequiredEnergy", 0);
+	lua_Number EarliestRound = ReadLuaTableNumber(L, "EarliestRound", 0);
+	lua_Number ID = ReadLuaTableNumber(L, "id", 0);
+
+	write_debug(AddAttack, "Recieved attack of value { %s, %d, %d, %d }",
+			AttackDispName, (int)RequiredEnergy, (int)EarliestRound, (int)ID);
+
+	lua_getfield(L, -3, "current_entries");
+
+	lua_newtable(L);
+	
+	type = lua_getfield(L, -3, "CanAttack");
+	if (type != LUA_TFUNCTION) {
+		lua_pop(L, 1);
+		lua_pushnil(L);
+	}
+
+	type = lua_getfield(L, -4, "AttackHandler");
+	if (type != LUA_TFUNCTION) {
+		lua_pop(L, 1);
+		lua_pushnil(L);
+	}
+
+//	return 0;
+
+	lua_setfield(L, -4, "attack_handler");
+	lua_setfield(L, -3, "can_attack");
+
+	lua_pushnumber(L, ID);
+	lua_setfield(L, -2, "id");
+
+	lua_pushnumber(L, EarliestRound);
+	lua_setfield(L, -2, "first_round");
+
+	lua_pushnumber(L, RequiredEnergy);
+	lua_setfield(L, -2, "minimum_energy");
+
+	lua_pushstring(L, AttackDispName);
+	lua_setfield(L, -2, "disp_name");
+
+	lua_pushstring(L, AttackName);
+	lua_setfield(L, -2, "int_name");
+
+	int len = lua_rawlen(L, -2);	// #current_entries
+	lua_rawseti(L, -2, len+1);
+
+	lua_pop(L, 1);
+
 	return 0;
 }
 
 int CreateAttackPlugin(lua_State *L) {
-	//int n = lua_gettop(L);
+	int n = lua_gettop(L);
 
+//	lua_pushnumber(L, 0);
+//	return 1;
+	
 	luaL_Reg fns[] = {
 		{ .name="AddAttack", .func=AddAttack },
 		{ NULL, NULL }
@@ -35,8 +111,6 @@ int CreateAttackPlugin(lua_State *L) {
 
 	lua_newtable(L);
 	lua_setfield(L, -2, "current_entries");
-
-	luaL_setfuncs(L, fns, 0);
 
 	return 1;
 }
