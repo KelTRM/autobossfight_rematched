2b22fce3b9c8955032fddc54be69df22310d8830
Kelvin Hansen <kelvin@keltrm.com>
2026-07-26 16:40:49 -0500

    remove debug statement in PushLuaArray

--- loadlua/push_lua_array.c
+++ loadlua/push_lua_array.c
@@ -4,9 +4,7 @@
 // pushes table into array at end of stack
 int PushLuaArray(lua_State *L, int ArrayIndex) {
 	size_t Length = lua_rawlen(L, ArrayIndex);
-
-	write_debug(PushLuaArray, "Writing data to index %d.", Length+1);
-
+//	write_debug(PushLuaArray, "Writing data to index %d.", Length+1);
 	lua_rawseti(L, ArrayIndex, Length+1);
 	return 1;
 }
