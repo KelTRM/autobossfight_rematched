27548fd3f539d6a9faca5f2b9ced24c3421c1801
Kelvin Hansen <kelvin@keltrm.com>
2026-08-01 22:47:18 -0500

    further work on ConvertTableToAttack

--- loadlua/attacks/register_lua_attack.c
+++ loadlua/attacks/register_lua_attack.c
@@ -1,6 +1,11 @@
+#define USE_STD_STRLEN
+
 // oh fuck this is gonna be a pain.
 #include"lua_attack.h"
 #include"../../attacks/attacks.h"
+#include"../debug/debug.h"
+#include<stdlib.h>
+#include<string.h>
 #include<lua.h>
 
 struct LuaAttack_t {
@@ -42,24 +47,55 @@ struct LuaAttack_t {
 	lua_pushvalue(L, -2);
 */
 
+const char *ReadLuaTableString(lua_State *L, const char *Name, char *DefaultValue);
+lua_Number ReadLuaTableNumber(lua_State *L, const char *Name, lua_Number DefaultValue);
+
 Attack_t ConvertTableToAttack(lua_State *L, int idx) {
 	idx = lua_absindex(L, idx);
 
-	if (!lua_istable(L, idx)) {
-		lua_pushstring(L, "recieved non-table as attack.");
+//	if (!lua_istable(L, idx)) {
+//		lua_pushstring(L, "recieved non-table as attack.");
+//		lua_error(L);
+//	}
+
+//	Attack_t Attack = { 0 };
+
+//	int type = lua_getfield(L, idx, "id");
+//	if (type != LUA_TNUMBER) {
+//		lua_pushstring(L, "recieved field id of type other than number");
+//		lua_error(L);
+//	}
+//	Attack.ID = lua_tonumber(L, -1);
+
+//	type = lua_getfield(L, idx, "first_round");
+
+	AttackID_t ID = ReadLuaTableNumber(L, "id", 0);
+	lua_Number FirstRound = ReadLuaTableNumber(L, "first_round", 0);
+	lua_Number MinimumEnergy = ReadLuaTableNumber(L, "minimum_energy", -1);
+
+	const char *DisplayName = ReadLuaTableString(L, "disp_name", NULL);
+	const char *Identifier = ReadLuaTableString(L, "int_name", NULL);
+
+	if (DisplayName == NULL) {
+		lua_pushstring(L, "invalid attack name.");
 		lua_error(L);
 	}
 
-	Attack_t Attack = { 0 };
-
-	int type = lua_getfield(L, idx, "id");
-	if (type != LUA_TNUMBER) {
-		lua_pushstring(L, "recieved field id of type other than number");
+	if (Identifier == NULL) {
+		lua_pushstring(L, "invalid attack identifier");
 		lua_error(L);
 	}
-	Attack.ID = lua_tonumber(L, -1);
 
-//	lua_pushvalue(L, idx);
+	write_debug(DEBUG_MODE, "Registering attack \"%s\" of { \"%s\", %d, %d, %d }");
+
+	Attack_t LuaAttack = { 0 };
+
+	LuaAttack.ID = ID;
+	LuaAttack.FirstAvailableRound = FirstRound;
+	LuaAttack.MinimumEnergy = MinimumEnergy;
+	
+	LuaAttack.Identifier = malloc(strlen(Identifier));
+	LuaAttack.AttackName = malloc(strlen(DisplayName));
 
 	return (Attack_t){ 0 };
 }
