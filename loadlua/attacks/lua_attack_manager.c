257ade2267153b63ebf0bb04acf2833940da3504
Kelvin Hansen <kelvin@keltrm.com>
2026-08-17 13:14:06 -0500

    delete and update comments in lua_attack_manager

--- loadlua/attacks/lua_attack_manager.c
+++ loadlua/attacks/lua_attack_manager.c
@@ -9,14 +9,9 @@ typedef uint32_t	PluginID_t;
 typedef int64_t		BlockID_t;
 
 // todos:
-// implement plugin handle system
-// implement plugin attack size handler
-// implement interface for adding/removing plugin attacks
-
-// # of IDs allocated at a time
-// #define ATTACK_BLOCK_SIZE		10
-// #define MAX_ATTACKS_DEFAULT		65535
-// #define MAX_PLUGINS			32767
+// implement plugin handle system - done
+// implement plugin attack size handler - done
+// implement interface for adding/removing plugin attacks - half done
 
 #define PLUGIN_DEFINED			0xFFFFFFFFul
 #define INVALID_PLUGIN_ID		0xFFFFFFFFul
