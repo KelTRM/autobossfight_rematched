1b91e2fc48dce30aed6f4c4ba7d41c69759503d9
Kelvin Hansen <kelvin@keltrm.com>
2026-08-22 14:49:40 -0500

    add function to get an entity from a lua table

--- loadlua/types/lua_entity.c
+++ loadlua/types/lua_entity.c
@@ -53,3 +53,56 @@ void CreateEntityTable(lua_State *L, Entity_t *Entity) {
 	lua_pushnumber(L, Entity->HealthPoints);
 	lua_setfield(L, -2, "hp");
 }
+
+// gets entity from top of table, converts it to C entity
+Entity_t GetEntityFromTable(lua_State *L) {
+	// this code is very DRY (do repeat yourself)
+	Entity_t Entity = { 0 };
+
+	// name
+	lua_getfield(L, -1, "name");
+	Entity.Name = lua_tostring(L, -1);
+	lua_pop(L, 1);
+
+	// color
+	lua_getfield(L, -1, "color");
+
+	lua_getfield(L, -1, "r");
+	Entity.EntityColor.r = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	lua_getfield(L, -2, "g");
+	Entity.EntityColor.g = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	lua_getfield(L, -3, "b");
+	Entity.EntityColor.b = lua_tonumber(L, -1);
+	lua_pop(L, 2);
+
+	// attack
+	lua_getfield(L, -1, "attack");
+	Entity.Attack = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	lua_getfield(L, -1, "energy");
+	Entity.Energy = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	lua_getfield(L, -1, "healing_minimum");
+	Entity.HealingMinimum = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	lua_getfield(L, -1, "healing_maximum");
+	Entity.HealingMaximum = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	lua_getfield(L, -1, "hp");
+	Entity.HealthPoints = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+
+	return Entity;
+}
+
+
+
+
--- loadlua/types/lua_types.h
+++ loadlua/types/lua_types.h
@@ -4,3 +4,5 @@
 #include"../entity.h"
 
 void CreateEntityTable(lua_State *L, Entity_t *Entity);
+Entity_t GetEntityFromTable(lua_State *L);
+
